62f8a3a4b1ff443eaecb74b224298e59ce73b9af
Ida Johansson <ida.johansson90@hotmail.com>
2024-02-07 14:38:13 +0100

    Project setup - first commit

---
+++ kupong/src/main/java/com/example/kupong/KupongApplication.java
@@ -0,0 +1,13 @@
+package com.example.kupong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KupongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KupongApplication.class, args);
+	}
+
+}
---
+++ kupong/src/test/java/com/example/kupong/KupongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kupong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KupongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
