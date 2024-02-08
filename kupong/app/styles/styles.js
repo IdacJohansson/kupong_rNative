2a375d4e7d9bfb67233ba2a48b2f1edfa4caf66c
Ida Johansson <ida.johansson90@hotmail.com>
2024-02-08 09:22:54 +0100

    Backend_setUp

---
+++ kupong/app/styles/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const overallStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "gray",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+
